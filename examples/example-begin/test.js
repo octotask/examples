af1f0e2ef9abef92a9c4f40d1135731ec9e5949e
fortishield <161459699+FortiShield@users.noreply.github.com>
2024-11-17 21:57:34 +0000

    update

---
+++ examples/example-begin/test.js
@@ -0,0 +1,62 @@
+const { suite } = require('uvu')
+const assert = require('uvu/assert')
+
+const nock = require('nock')
+nock.disableNetConnect()
+
+const { Octotask, OctotaskOctokit } = require('octotask')
+
+const app = require('./src/http/post-api-github-webhooks/app')
+
+/** @type {import('octotask').Octotask */
+let octotask
+const test = suite('app')
+test.before.each(() => {
+  octotask = new Octotask({
+    // simple authentication as alternative to appId/privateKey
+    githubToken: 'test',
+    // disable logs
+    logLevel: 'warn',
+    // disable request throttling and retries
+    Octokit: OctotaskOctokit.defaults({
+      throttle: { enabled: false },
+      retry: { enabled: false },
+    }),
+  })
+  octotask.load(app)
+})
+
+test('recieves issues.opened event', async function () {
+  const mock = nock('https://api.github.com')
+    // create new check run
+    .post(
+      '/repos/octotask/example-aws-lambda-serverless/issues/1/comments',
+      (requestBody) => {
+        assert.equal(requestBody, { body: 'Hello, World!' })
+
+        return true
+      }
+    )
+    .reply(201, {})
+
+  await octotask.receive({
+    name: 'issues',
+    id: '1',
+    payload: {
+      action: 'opened',
+      repository: {
+        owner: {
+          login: 'octotask',
+        },
+        name: 'example-aws-lambda-serverless',
+      },
+      issue: {
+        number: 1,
+      },
+    },
+  })
+
+  assert.equal(mock.activeMocks(), [])
+})
+
+test.run()
